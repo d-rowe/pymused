0928c0048738b8f117f653631ea70f38e9bcb630
Daniel Rowe <danielrowetech@gmail.com>
2019-09-18 17:01:26 -0700

    Small clean

--- pymused/knowledge.py
+++ pymused/knowledge.py
@@ -1,24 +1,6 @@
 interval_semitones = [0, 2, 4, 5, 7, 9, 11]  # Semitones away from root for Major scale
 interval_types = ['P', 'M', 'M', 'P', 'P', 'M', 'M']  # Major scale interval qualities
-quality_offsets = {  # Interval types and their offset in semitones from reference
-    'P': {
-        -2: 'dd',
-        -1: 'd',
-        0: 'P',
-        1: 'A',
-        2: 'AA'
-    },
-    'M': {
-        -3: 'dd',
-        -2: 'd',
-        -1: 'm',
-        0: 'M',
-        1: 'A',
-        2: 'AA'
-    }
-}
 letters = 'CDEFGAB'
-accidentals = {'bbb': -3, 'bb': -2, 'b': -1, '#': 1, '##': 2, '###': 3, 'x': 2}
 
 
 def add_coords(coord1: [int, int], coord2: [int, int]) -> [int, int]:
