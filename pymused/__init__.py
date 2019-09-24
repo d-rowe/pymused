a79cd760b801d0573fa8a26d7989f769d6567beb
Daniel Rowe <danielrowetech@gmail.com>
2019-09-24 00:46:13 -0700

    cleaned, added Chord

--- pymused/__init__.py
+++ pymused/__init__.py
@@ -1,3 +1,4 @@
 from .interval import Interval
 from .pitch import Pitch
 from .scale import Scale
+from .chord import Chord
--- pymused/utils/knowledge.py
+++ pymused/utils/knowledge.py
@@ -1,9 +1,7 @@
-# Basic pitch and interval positional data
 letters = 'CDEFGAB'
 interval_semitones = [0, 2, 4, 5, 7, 9, 11]  # Semitones away from root for Major scale
 interval_types = ['P', 'M', 'M', 'P', 'P', 'M', 'M']  # Major scale interval qualities
 
-# Basic scale data
 scale_recipes = {
     'major': ['P1', 'M2', 'M3', 'P4', 'P5', 'M6', 'M7'],
     'natural minor': ['P1', 'M2', 'm3', 'P4', 'P5', 'm6', 'm7'],
@@ -19,4 +17,20 @@ scale_recipes = {
     'mixolydian': ['P1', 'M2', 'M3', 'P4', 'P5', 'M6', 'm7'],
     'aeolian': ['P1', 'M2', 'm3', 'P4', 'P5', 'm6', 'm7'],
     'locrian': ['P1', 'm2', 'm3', 'P4', 'd5', 'm6', 'm7'],
+}
+
+chord_recipes = {
+    'maj': ['P1', 'M3', 'P5'],
+    'min': ['P1', 'm3', 'P5'],
+    'aug': ['P1', 'M3', 'A5'],
+    'dim': ['P1', 'm3', 'd5'],
+    'dom7': ['P1', 'M3', 'P5', 'm7'],
+    'min7': ['P1', 'm3', 'P5', 'm7'],
+    'maj7': ['P1', 'M3', 'P5', 'M7'],
+    'aug7': ['P1', 'M3', 'A5', 'm7'],
+    'dim7': ['P1', 'm3', 'd5', 'd7'],
+    'm7dim5': ['P1', 'm3', 'd5', 'm7'],
+    'sus2': ['P1', 'P5', 'P8', 'M2'],
+    'sus4': ['P1', 'P5', 'P8', 'P4'],
+    'open5': ['P1', 'P5', 'P8']
 }
